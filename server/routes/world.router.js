14f15d0ad028985278d68f85ed4a17019e4f8b27
Mark Klein <kleincentral@gmail.com>
2024-01-02 11:52:35 -0600

    Created a World Router, with a reducer to display the data on User Home

---
+++ server/routes/world.router.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const {
+  rejectUnauthenticated,
+} = require("../modules/authentication-middleware");
+const pool = require("../modules/pool");
+
+const router = express.Router();
+
+// Handles Ajax request for user information if user is authenticated
+router.get("/", rejectUnauthenticated, (req, res) => {
+  // Send back world object from the session (previously queried from the database)
+  const queryText = `
+    SELECT * FROM "worlds"
+    WHERE "worlds".user_id = $1;`;
+  pool
+    .query(queryText, [req.user.id])
+    .then((result) => res.send(result.rows))
+    .catch((err) => {
+      console.log("World get failed: ", err);
+      res.sendStatus(500);
+    });
+});
+
+module.exports = router;
